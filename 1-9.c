e3721001db117823d0f4dc896b2a10ffc51321de
sojsnake <93419110+sojsnake@users.noreply.github.com>
2021-11-14 13:15:30 +0900

    bweek1

---
+++ 1-9.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+    int A, B;
+    scanf("%d", &A);
+    scanf("%d", &B);
+    printf("%d\n", A + B);
+    printf("%d\n", A - B);
+    printf("%d\n", A * B);
+    printf("%d\n", A / B);
+    printf("%d\n", A % B);
+
+    return 0;
+}
